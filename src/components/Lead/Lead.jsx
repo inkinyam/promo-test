61d1998449247a71eae54981d618ddc5b8df8c7a
inki <inkinyam@yandex.ru>
2023-08-03 15:15:35 +0300

    fix: тексты faq + даты

--- src/components/Lead/Lead.jsx
+++ src/components/Lead/Lead.jsx
@@ -23,7 +23,7 @@ useLayoutEffect(()=> {
         <div className="lead__textblock">
           <h1 className='lead__title'>Марафон кофе</h1>
           <p className="lead__description">Покупайте кофейные напитки* на АЗС «ЛУКОЙЛ» и получайте 1500 баллов на карту программы лояльности</p>
-          <p className="lead__dates">01.08.2023 – 31.11.2023</p>
+          <p className="lead__dates">05.08.2023 – 30.11.2023</p>
         </div>
         <div className="lead__imgbox">
           <div className="container">
